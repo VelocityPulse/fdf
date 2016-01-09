f1244b6446d899a66d42ec9cf2db508c29bd28c2
Velocitypulse <velocitypulse@outlook.fr>
2016-01-09 15:29:32 +0100

    retrait de la lib stdio pour les tests

--- draw/draw_pixel.c
+++ draw/draw_pixel.c
@@ -6,13 +6,11 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/04 17:30:22 by                   #+#    #+#             */
-/*   Updated: 2016/01/08 13:45:59 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/01/09 15:29:04 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/draw.h"
-
-#include <stdio.h>
 
 void	ft_draw_pixel(t_mlx *mlx, int color, t_pt pt)
 {
@@ -21,9 +19,6 @@ void	ft_draw_pixel(t_mlx *mlx, int color, t_pt pt)
 
 	octet = mlx->mlx_img->bpp / 8;
 	position = (pt.y * mlx->mlx_img->width) + (pt.x * octet);
-	
-	printf("x;y -> %d;%d\n", pt.x, pt.y);
-
 	if (ft_memcmp(mlx->mlx_img->data + position, &color, (unsigned)octet) != 0)
 		ft_memcpy(mlx->mlx_img->data + position, &color, (unsigned)octet);
 }
