18057c1fc998492a097c147948df353b87685d55
Velocitypulse <velocitypulse@outlook.fr>
2016-01-13 22:26:30 +0100

    finition de convert_to_array

--- sources/convert_to_array.c
+++ sources/convert_to_array.c
@@ -6,26 +6,38 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/13 15:53:52 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/01/13 16:12:40 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/01/13 22:26:11 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../headers/headers.h"
+#include "../headers/header.h"
 
 t_array		*ft_convert_to_array(char **tab, int nbr_line)
 {
-	char	**tab_values;
-	int 	i;
-	int		i2
+	t_array		*array;
+	char		**tab_values;
+	int 		i;
+	int			i2;
+	int			cpt;
 
-	i = 0;
-	while (i <= nbr_line)
+	i = -1;
+	array = (t_array *)malloc(sizeof(t_array));
+	array->tab = (int **)ft_memalloc(sizeof(int *) * nbr_line);
+	array->size.y = nbr_line;
+	while (++i <= nbr_line)
 	{
 		tab_values = ft_strsplit(tab[i], ' ');
-		i2 = 0;
-		while (tab_values[i2])
-		{
-			
-		}
+		cpt = 0;
+		while (tab_values[cpt])
+			cpt++;
+		array->tab[i] = (int *)ft_memalloc(sizeof(int) * (cpt + 1));
+		i2 = -1;
+		while (tab_values[++i2])
+			array->tab[i][i2] = ft_atoi(tab_values[i2]);
+		i2++;
+		while (tab_values[--i2])
+			ft_memdel((void **)&tab_values[i2]);
+		ft_memdel((void **)tab_values);
 	}
+	return (array);
 }
--- sources/get_array.c
+++ sources/get_array.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/13 13:21:06 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/01/13 15:53:35 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/01/13 21:00:56 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ t_array		*ft_get_array(int fd, t_array *array)
 	tab = ft_export_gline(list_line);
 	list_line = ft_free_gline(list_line);
 	if (ft_check_tab(tab, nbr_line))
-		array = ft_convert_to_array(tab);
+		array = ft_convert_to_array(tab, nbr_line);
 	ft_free_tab(tab, nbr_line);
 	return (array);
 }
