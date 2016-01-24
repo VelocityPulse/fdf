da222d6c8624e0d01ee00b85e7e3bf02782f7727
Velocitypulse <velocitypulse@outlook.fr>
2016-01-24 18:29:32 +0100

    petite opti pour draw_array_layout

--- draw/draw_array_layout.c
+++ draw/draw_array_layout.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/23 15:31:15 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/01/23 15:38:41 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/01/24 18:29:15 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,9 @@ t_pt	*ft_array_layout(t_pt *pts, int size, t_matrix t)
 	int		i;
 	t_pt	*new_pts;
 
-	i = 0;
+	i = 1;
 	new_pts = (t_pt *)malloc(sizeof(t_pt) * size);
-	while (i <= size)
-	{
+	while (++i <= size)
 		new_pts[i] = ft_multiply_matrix(pts[i], t);
-		i++;
-	}
 	return (new_pts);
 }
