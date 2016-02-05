b844c9136028469c2822e0c8d3029ccea3f793e8
Velocitypulse <velocitypulse@outlook.fr>
2016-02-05 17:48:16 +0100

    recadrage de l'affichage initial

--- sources/edit_reset.c
+++ sources/edit_reset.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/03 23:33:51 by                   #+#    #+#             */
-/*   Updated: 2016/02/05 12:01:39 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/02/05 17:48:12 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_edit_reset(t_info *info)
 	{
 		info->scale = ft_make_pt3d(20, 20, 4);
 		info->rad = ft_make_vector(0, 0, 0);
-		info->pos = ft_make_pt(400, 350);
+		info->pos = ft_make_pt(500, 350);
 		info->array = ft_convert_array_to_pts(info->array);
 		ft_fdf_add_scale(info->array, info->scale);
 	}
