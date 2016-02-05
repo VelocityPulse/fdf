71903cff6c1e503db918156d6dc00e199da018e9
Velocitypulse <velocitypulse@outlook.fr>
2016-02-05 17:47:58 +0100

    recadrage de l'affichage initial

--- sources/init_info.c
+++ sources/init_info.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/02 15:57:11 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/02/05 17:12:17 by                  ###   ########.fr       */
+/*   Updated: 2016/02/05 17:47:36 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ t_info		*ft_init_info(t_mlx *mlx, t_array *a)
 	info->key = -1;
 	info->scale = ft_make_pt3d(20, 20, 4);
 	info->rad = ft_make_vector(0, 0, 0);
-	info->pos = ft_make_pt(400, 350);
+	info->pos = ft_make_pt(500, 350);
 	return (info);
 }
