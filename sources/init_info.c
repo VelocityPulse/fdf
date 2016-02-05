c26a05bef6a062b1cd1f69e0c445f757785bb38b
Velocitypulse <velocitypulse@outlook.fr>
2016-02-05 17:13:18 +0100

    correction du fameux bug qui placais d'un cran a gauche la fenetre des le depart

--- sources/init_info.c
+++ sources/init_info.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/02 15:57:11 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/02/03 22:20:15 by                  ###   ########.fr       */
+/*   Updated: 2016/02/05 17:12:17 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ t_info		*ft_init_info(t_mlx *mlx, t_array *a)
 	info = (t_info *)ft_memalloc(sizeof(t_info));
 	info->mlx = mlx;
 	info->array = a;
-	info->key = 0;
+	info->key = -1;
+	info->scale = ft_make_pt3d(20, 20, 4);
 	info->rad = ft_make_vector(0, 0, 0);
 	info->pos = ft_make_pt(400, 350);
-	info->scale = ft_make_pt3d(20, 20, 4);
 	return (info);
 }
