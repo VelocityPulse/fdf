d1b83d000ad054c6e83284feeb547c65db5c1c27
Velocitypulse <velocitypulse@outlook.fr>
2016-02-07 15:16:08 +0100

    ajout des autres fonctions draw_color_line

--- draw/draw_color_line.c
+++ draw/draw_color_line.c
@@ -6,11 +6,21 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/06 12:32:21 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/02/07 14:40:50 by                  ###   ########.fr       */
+/*   Updated: 2016/02/07 15:15:23 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/draw.h"
+
+static t_rgb	ft_increment_rgb(t_dual_color c, int n, int x)
+{
+	t_rgb	d;
+
+	d.r = c.c1.r + (int)(((float)(c.c2.r - c.c1.r) / (float)n) * (float)x);
+	d.g = c.c1.g + (int)(((float)(c.c2.g - c.c1.g) / (float)n) * (float)x);
+	d.b = c.c1.b + (int)(((float)(c.c2.b - c.c1.b) / (float)n) * (float)x);
+	return (d);
+}
 
 void	ft_draw_horizontal_c(t_line l, t_mlx *mlx, int var, t_dual_color c)
 {
@@ -18,16 +28,62 @@ void	ft_draw_horizontal_c(t_line l, t_mlx *mlx, int var, t_dual_color c)
 	int		n;
 	int		x;
 
-	n = l.dx + l.dy;
+	if (l.dx < 0)
+		l.dx = -l.dx;
+	n = l.dx;
 	x = 0;
 	while (l.start.x != l.end.x)
 	{
-		x++;
-		d.r = c.c1.r + (int)(((float)(c.c2.r - c.c1.r) / (float)n) * (float)x);
-		d.g = c.c1.g + (int)(((float)(c.c2.g - c.c1.g) / (float)n) * (float)x);
-		d.b = c.c1.b + (int)(((float)(c.c2.b - c.c1.b) / (float)n) * (float)x);
+		d = ft_increment_rgb(c, n, x++);
 		ft_draw_pixel(mlx, ft_get_hexa(d), l.start);
 		l.start.x += var;
+	}
+}
+
+void	ft_draw_vertical_c(t_line l, t_mlx *mlx, int var, t_dual_color c)
+{
+	t_rgb	d;
+	int		n;
+	int		x;
+
+	if (l.dy < 0)
+		l.dy = -l.dy;
+	n = l.dy;
+	x = 0;
+	while (l.start.y != l.end.y)
+	{
+		d = ft_increment_rgb(c, n, x++);
+		ft_draw_pixel(mlx, ft_get_hexa(d), l.start);
+		l.start.y += var;
+	}
+}
+
+void	ft_draw_bresenham_c(t_line l, t_mlx *mlx, t_pt var, t_dual_color c)
+{
+	t_rgb	d;
+	int		err;
+	int		n;
+	int		x;
+
+	if (l.dx < 0)
+		l.dx = -l.dx;
+	if (l.dy < 0 && !(x = 0))
+		l.dy = -l.dy;
+	n = l.dx + l.dy;
+	err = -l.dx>>1;
+	while (l.start.x != l.end.x)
+	{
+		d = ft_increment_rgb(c, n, x++);
+		ft_draw_pixel(mlx, ft_get_hexa(d), l.start);
+		err += l.dy;
+		while (err > 0)
+		{
+			d = ft_increment_rgb(c, n, x++);
+			ft_draw_pixel(mlx, ft_get_hexa(d), l.start);
+			err -= l.dx;
+			l.start.y += var.y;
+		}
+		l.start.x += var.x;
 	}
 }
 
@@ -44,9 +100,8 @@ void	ft_draw_color_line(t_line l, t_mlx *mlx, int c1, int c2)
 		ft_draw_pixel(mlx, c1, l.start);
 	else if (l.dy == 0)
 		ft_draw_horizontal_c(l, mlx, variance.x, c);
-/*	else if (l.dx == 0)
+	else if (l.dx == 0)
 		ft_draw_vertical_c(l, mlx, variance.y, c);
 	else
 		ft_draw_bresenham_c(l, mlx, variance, c);
-*/
 }
--- maintest/main.c
+++ maintest/main.c
@@ -6,40 +6,40 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/05 13:40:00 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/02/07 14:38:17 by                  ###   ########.fr       */
+/*   Updated: 2016/02/07 15:12:39 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/header.h"
 
 /*
-int		main()
-{
-	t_mlx *mlx;
-	t_line line;
+   int		main()
+   {
+   t_mlx *mlx;
+   t_line line;
 
-	mlx = NULL;
-	if (!(mlx = (t_mlx *)ft_memalloc(sizeof(t_mlx))))
-		return (0);
-	ft_mlx_init(600, 550, mlx, "fdf");
+   mlx = NULL;
+   if (!(mlx = (t_mlx *)ft_memalloc(sizeof(t_mlx))))
+   return (0);
+   ft_mlx_init(600, 550, mlx, "fdf");
 
-	ft_draw_line(ft_make_line(599, 0, 200, 350), mlx, (int)0xffffff);
-	ft_draw_line(ft_make_line(599, 0, 200, 400), mlx, (int)0x00ff00);
-	ft_draw_line(ft_make_line(300, 200, 300, 400), mlx, (int)0xaa00ff);
-	ft_draw_line(ft_make_line(0, 0, 580, 480), mlx, (int)0xff0000);
-	ft_draw_line(ft_make_line(450, 450, 320, 300), mlx, (int)0xaa00ff);
-	ft_draw_rect(ft_make_line(100, 450, 200, 500), mlx, (int)0x0000ff);
-	ft_draw_rect(ft_make_line(210, 510, 110, 460), mlx, (int)0x00ff00);
+   ft_draw_line(ft_make_line(599, 0, 200, 350), mlx, (int)0xffffff);
+   ft_draw_line(ft_make_line(599, 0, 200, 400), mlx, (int)0x00ff00);
+   ft_draw_line(ft_make_line(300, 200, 300, 400), mlx, (int)0xaa00ff);
+   ft_draw_line(ft_make_line(0, 0, 580, 480), mlx, (int)0xff0000);
+   ft_draw_line(ft_make_line(450, 450, 320, 300), mlx, (int)0xaa00ff);
+   ft_draw_rect(ft_make_line(100, 450, 200, 500), mlx, (int)0x0000ff);
+   ft_draw_rect(ft_make_line(210, 510, 110, 460), mlx, (int)0x00ff00);
 
-	line = ft_make_line(45, 100, 205, 400);
-	ft_draw_losange(ft_make_losange(line, 56), mlx, (int)0xff00ff);
-	line = ft_make_line(100, 50, 310, 200);
-	ft_draw_isocele(ft_make_isocele(line, -100), mlx, (int)0xff0000);
-	mlx_put_image_to_window(mlx->p_mlx, mlx->p_win, mlx->mlx_img->p_img, 0, 0);
-	mlx_loop(mlx->p_mlx);
-	return (0);
-}
-*/
+   line = ft_make_line(45, 100, 205, 400);
+   ft_draw_losange(ft_make_losange(line, 56), mlx, (int)0xff00ff);
+   line = ft_make_line(100, 50, 310, 200);
+   ft_draw_isocele(ft_make_isocele(line, -100), mlx, (int)0xff0000);
+   mlx_put_image_to_window(mlx->p_mlx, mlx->p_win, mlx->mlx_img->p_img, 0, 0);
+   mlx_loop(mlx->p_mlx);
+   return (0);
+   }
+   */
 /*
    int main()
    {
@@ -59,26 +59,23 @@ int		main()
 
 int		main()
 {
-	t_line	line;
-	t_mlx	*mlx;
-	t_pt	p1;
+	t_mlx			*mlx;
+	t_line			line;
 
 	mlx = NULL;
-	mlx = ft_mlx_init(400, 400, mlx, "test");
-	p1 = ft_make_pt(250, 100);
-//	while (p1.y < 200)
-//	{
-		line = ft_make_line(100, p1.y, p1.x, p1.y);
-		ft_draw_color_line(line, mlx, 0xff0000, 0xab32);
-//		p1.y++;
-//	}
-	p1 = ft_make_pt(350, 130);
-//		while (p1.y < 200)
-//	{
-		line = ft_make_line(300, p1.y, p1.x, p1.y);
-		ft_draw_color_line(line, mlx, 0xff0000, 0xab32);
-//		p1.y++;
-//	}
+	mlx = ft_mlx_init(600, 600, mlx, "test");
+
+	ft_draw_color_line(ft_make_line(599, 0, 200, 350), mlx, 0xff0000, 0x00ff00);
+	ft_draw_color_line(ft_make_line(599, 0, 200, 400), mlx, 0xff0000, 0x00ff00);
+	ft_draw_color_line(ft_make_line(300, 200, 300, 400), mlx, 0xff0000, 0x00ff00);
+	ft_draw_color_line(ft_make_line(0, 0, 580, 480), mlx, 0xff0000, 0x00ff00);
+	ft_draw_color_line(ft_make_line(450, 450, 320, 300), mlx, 0xff0000, 0x00ff00);
+
+   line = ft_make_line(45, 100, 205, 400);
+   ft_draw_losange(ft_make_losange(line, 56), mlx, (int)0xff00ff);
+   line = ft_make_line(100, 50, 310, 200);
+   ft_draw_isocele(ft_make_isocele(line, -100), mlx, (int)0xff0000);
+
 	ft_flush_image(mlx);
 	mlx_loop(mlx->p_mlx);
 }
