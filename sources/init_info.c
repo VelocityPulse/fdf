64d7b59941d5f4a1fc9d678ac443a098b9ea766a
Clement CHAMEYRAT <cchameyr@e3r10p12.42.fr>
2016-02-12 12:09:00 +0100

    recadrage de la fenetre dans init_info termine mais reste a faire dans edit_reset

--- sources/init_info.c
+++ sources/init_info.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/08 12:06:01 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/02/11 17:06:02 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/02/12 12:08:28 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 t_info		*ft_init_info(t_mlx *mlx, t_array *a)
 {
 	t_info		*info;
+	int			graphic_size;
 
 	info = (t_info *)ft_memalloc(sizeof(t_info));
 	info->mlx = mlx;
 	info->array = a;
 	info->key = -1;
-	info->scale = ft_make_pt3df(mlx->width / (a->max_size.x /** 2*/), mlx->width / (a->max_size.x /** 2*/),
-			4);
+	graphic_size = mlx->width / (a->max_size.x * 2);
+	info->scale = ft_make_pt3df(graphic_size, graphic_size, 4);
 	info->rad = ft_make_vector(0, 0, 0);
 	info->pos = ft_make_pt(mlx->width / 2, mlx->height / 2);
 	return (info);
