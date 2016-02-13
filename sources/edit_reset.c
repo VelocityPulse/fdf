b2a1d10653e5a250c046e6430019c5b91dc88e7a
Clement CHAMEYRAT <cchameyr@e3r7p12.42.fr>
2016-02-13 18:15:08 +0100

    patch sur le reset de la size avec la touche espace

--- sources/edit_reset.c
+++ sources/edit_reset.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/08 12:06:24 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/02/13 12:44:50 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/02/13 18:14:36 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 void	ft_edit_reset(t_info *info)
 {
-	int		graphic_size;
+	float	graphic_size;
+	int		x;
 
 	if (info->key == 49)
 	{
 		info->array = ft_convert_array_to_pts(info->array);
-		graphic_size = info->mlx->width / (info->array->max_size.x * 2);
+		x = info->array->max_size.x;
+		graphic_size = (float)info->mlx->width / ((float)x * 2);
 		info->scale = ft_make_pt3df(graphic_size, graphic_size, 4);
 		info->rad = ft_make_vector(0, 0, 0);
 		info->pos = ft_make_pt(info->mlx->width / 2, info->mlx->height / 2);
